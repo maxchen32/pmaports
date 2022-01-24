25ee0f090e08d24c1a593c46f52395c28d38e12c
Caleb Connolly <caleb@connolly.tech>
2022-01-24 00:33:28 +0000

    main/postmarketos-mkinitfs: switch to pbsplash (MR 2878)
    This modifies to the postmarketOS ramdisk init to use the new pbsplash
    tool for displaying splash messages, replacing the old fbsplash.
    
    This also moves the show_splash call to run as soon as possible rather
    than waiting for mount_subpartitions to run (which can take a while).

--- main/postmarketos-mkinitfs/init_functions.sh
+++ main/postmarketos-mkinitfs/init_functions.sh
@@ -239,7 +239,7 @@ mount_boot_partition() {
 	partition=$(find_boot_partition)
 	if [ -z "$partition" ]; then
 		echo "ERROR: boot partition not found!"
-		show_splash /splash-noboot.ppm.gz
+		show_splash "ERROR: Boot partition not found\\nhttps://postmarketos.org/troubleshooting"
 		loop_forever
 	fi
 
@@ -276,7 +276,7 @@ extract_initramfs_extra() {
 	initramfs_extra="$1"
 	if [ ! -e "$initramfs_extra" ]; then
 		echo "ERROR: initramfs-extra not found!"
-		show_splash /splash-noinitramfsextra.ppm.gz
+		show_splash "ERROR: initramfs-extra not found\\nhttps://postmarketos.org/troubleshooting"
 		loop_forever
 	fi
 	echo "Extract $initramfs_extra"
@@ -285,7 +285,7 @@ extract_initramfs_extra() {
 
 wait_root_partition() {
 	while [ -z "$(find_root_partition)" ]; do
-		show_splash /splash-norootfs.ppm.gz
+		show_splash "ERROR: root partition not found\\nhttps://postmarketos.org/troubleshooting"
 		echo "Could not find the rootfs."
 		echo "Maybe you need to insert the sdcard, if your device has"
 		echo "any? Trying again in one second..."
@@ -367,13 +367,13 @@ unlock_root_partition() {
 		done
 		ROOT_PARTITION_UNLOCKED=1
 		# Show again the loading splashscreen
-		show_splash_loading
+		show_splash "Loading..."
 	fi
 }
 
 resize_root_filesystem() {
 	if [ "$ROOT_PARTITION_RESIZED" = 1 ]; then
-		show_splash /splash-resizefs.ppm.gz
+		show_splash "Resizing filesystem during initial boot..."
 		partition="$(find_root_partition)"
 		touch /etc/mtab # see https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=673323
 		type="$(get_partition_type "$partition")"
@@ -399,7 +399,7 @@ resize_root_filesystem() {
 				;;
 			*)	echo "WARNING: Can not resize '$type' filesystem ($partition)." ;;
 		esac
-		show_splash_loading
+		show_splash "Loading..."
 	fi
 }
 
@@ -427,7 +427,7 @@ mount_root_partition() {
 	esac
 	if ! [ -e /sysroot/usr ]; then
 		echo "ERROR: unable to mount root partition!"
-		show_splash /splash-mounterror.ppm.gz
+		show_splash "ERROR: unable to mount root partition\\nhttps://postmarketos.org/troubleshooting"
 		loop_forever
 	fi
 }
@@ -589,12 +589,12 @@ start_charging_mode() {
 	# Start it once and then start triggerhappy
 	(
 		charging-sdl -pcf "$fontpath" \
-			|| show_splash /splash-charging-error.ppm.gz
+			|| show_splash "Charging mode failed"
 	) &
 	thd --deviceglob /dev/input/event* --triggers /etc/triggerhappy.conf
 }
 
-# $1: path to ppm.gz file
+# $1: Message to show
 show_splash() {
 	# Skip for non-framebuffer devices
 	# shellcheck disable=SC2154
@@ -602,23 +602,19 @@ show_splash() {
 		echo "NOTE: Skipping framebuffer splashscreen (deviceinfo_no_framebuffer)"
 		return
 	fi
+
 	# Disable splash
 	if grep -q PMOS_NOSPLASH /proc/cmdline; then 
-         	return
+		return
 	fi
-	echo "IMG_ALIGN=CM" >>/tmp/fbsplash.cfg
-	gzip -c -d "$1" >/tmp/splash.ppm
-	fbsplash -s /tmp/splash.ppm -i /tmp/fbsplash.cfg
-}
 
-show_splash_loading() {
-	# Allow overriding the default loading splash screen with a
-	# "splash.ppm.gz" file on the boot partition
-	if [ -e /boot/splash.ppm.gz ]; then
-		show_splash /boot/splash.ppm.gz
-	else
-		show_splash /splash-loading.ppm.gz
-	fi
+	killall pbsplash 2>/dev/null
+
+	while pgrep pbsplash >/dev/null; do
+		sleep 0.01
+	done
+
+	/usr/bin/pbsplash -s /usr/share/pbsplash/pmos-logo-text.svg -m "$1 " &
 }
 
 set_framebuffer_mode() {
